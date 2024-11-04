98b82b63beff2ce1056b1261f710cd89f61b936b
lâm vũ <botay417@gmail.com>
2024-11-04 21:34:20 +0700

    add todocomponent

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a771ba335e5f63519fcadd1a7cb0b53513902a23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92ebcbbdac546222eb822550af1baf9806aa856")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
